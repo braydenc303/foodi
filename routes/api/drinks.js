e58bf78ae6fcb0131bb3a0170b6c54bda7c3c437
Brayden Copley <braydenc303@gmail.com>
2019-05-19 21:05:48 -0600

    added fnb to api routes

--- routes/api/drinks.js
+++ routes/api/drinks.js
@@ -1,24 +1,24 @@
 // Creates a new router object.
 const router = require("express").Router();
-// Require in the beers controller to give us access to all of the functionality of our database calls.
-const beersController = require("../../controllers/beersController");
+// Require in the drinks controller to give us access to all of the functionality of our database calls.
+const drinksController = require("../../controllers/drinksController");
 
-// Matches with "/api/beers"
-// "/api" comes from index.js on the mail level of routes. "/beers" comes from index.js inside of the api folder.
+// Matches with "/api/drinks"
+// "/api" comes from index.js on the mail level of routes. "/drinks" comes from index.js inside of the api folder.
 router.route("/")
-  // Lets us get all of the beers from the database
-  .get(beersController.findAll)
-  // Lets us post a beer to the database
-  .post(beersController.create);
+  // Lets us get all of the drinks from the database
+  .get(drinksController.findAll)
+  // Lets us post a drink to the database
+  .post(drinksController.create);
 
-// Matches with "/api/beers/:id"
+// Matches with "/api/drinks/:id"
 router.route("/:id")
-  // Lets us find a beer by a specified ID
-  .get(beersController.findById)
-  // Lets us update a beer with a specified ID
-  .put(beersController.update)
-  // Lets us delete a beer with a specified ID
-  .delete(beersController.remove);
+  // Lets us find a drink by a specified ID
+  .get(drinksController.findById)
+  // Lets us update a drink with a specified ID
+  .put(drinksController.update)
+  // Lets us delete a drink with a specified ID
+  .delete(drinksController.remove);
 
-// exports a router module that can be used in index.js inside the api routes folder thus giving index access to the beer api routes. 
+// exports a router module that can be used in index.js inside the api routes folder thus giving index access to the drink api routes. 
 module.exports = router;
